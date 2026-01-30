1a99402c48117286b968dacfb08aa25cedc32ae8
Chanon Pluemhathaikij <oscarchanon51715@gmail.com>
2026-01-30 20:59:10 +0700

    Delete mapper.py

--- mapper.py
+++
@@ -1,8 +0,0 @@
-
-import csv
-
-with open('spotify-data.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(f"{row['artists']}\t")
-#artists: index 2
